edebca6042a6ed8cc7afd69351fd5428cb2c4cb0
MiguelRamos-145 <miguel.arevalo@catolica.edu.sv>
2025-03-28 16:58:34 -0600

    Agregar archivos de proyecto.

---
+++ L01P02-2022AR652/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P02_2022AR652.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
